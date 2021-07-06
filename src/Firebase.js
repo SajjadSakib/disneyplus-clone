01372e604fbc3ae86a5b4f440e75ab859668de6a
SajjadSakib <67702070+SajjadSakib@users.noreply.github.com>
2021-07-06 17:47:09 +0600

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2btUar9Ml0z6Obc3SIjmSHR8l2-3P53c",
-  authDomain: "disneyplus-clone-1723.firebaseapp.com",
-  projectId: "disneyplus-clone-1723",
-  storageBucket: "disneyplus-clone-1723.appspot.com",
-  messagingSenderId: "377662093201",
-  appId: "1:377662093201:web:64c3b191075f07a132cc30",
-  measurementId: "G-ZMJ25R8TRJ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
